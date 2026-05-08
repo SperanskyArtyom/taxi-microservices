4d835fabec079b11be565037823dadf3d97d5a06
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 04:03:08 +0700

    feat: add passenger and driver entities with Liquibase migrations

---
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/domain/entity/Driver.java
@@ -0,0 +1,40 @@
+package io.github.speranskyatryom.taxi_microservices.user_service.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "drivers")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(name = "car_info", nullable = false)
+    private String carInfo;
+
+    @Column(name = "is_available")
+    private boolean isAvailable = false;
+
+    @Builder.Default
+    @Column(precision = 3, scale = 2, nullable = false)
+    private BigDecimal rating = BigDecimal.valueOf(5.0);
+}
