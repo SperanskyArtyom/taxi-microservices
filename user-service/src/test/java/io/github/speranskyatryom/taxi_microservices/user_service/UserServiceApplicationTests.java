7c9e43464ff6a21150a5a20a8bbab450a02a6667
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 00:36:37 +0700

    chore: initial Spring Boot user-service application and configure Liquibase master changelog

---
+++ user-service/src/test/java/io/github/speranskyatryom/taxi_microservices/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.speranskyatryom.taxi_microservices.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
