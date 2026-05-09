2242b16ab88fd461bf47f5799db337eb1312eb5b
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 11:33:17 +0700

    feat: add ResourceNotFoundException and its handler in GlobalExceptionHandler

--- user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/controller/GlobalExceptionHandler.java
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.github.speranskyatryom.taxi_microservices.user_service.controller;
 
 import io.github.speranskyatryom.taxi_microservices.user_service.exception.AlreadyExistsException;
+import io.github.speranskyatryom.taxi_microservices.user_service.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -16,6 +17,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<String> handleAlreadyExists(AlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
