5c7ddcfe074f30116deed3e75cc9a115a5476387
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 12:17:31 +0700

    feat: add new endpoints in controllers

--- user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/controller/DriverController.java
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/controller/DriverController.java
@@ -1,12 +1,13 @@
 package io.github.speranskyatryom.taxi_microservices.user_service.controller;
 
-import io.github.speranskyatryom.taxi_microservices.user_service.dto.DriverRegistrationRequest;
-import io.github.speranskyatryom.taxi_microservices.user_service.dto.DriverResponse;
+import io.github.speranskyatryom.taxi_microservices.user_service.dto.*;
 import io.github.speranskyatryom.taxi_microservices.user_service.service.DriverService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/drivers")
@@ -18,5 +19,31 @@ public class DriverController {
     @ResponseStatus(HttpStatus.CREATED)
     public DriverResponse register(@Valid @RequestBody DriverRegistrationRequest request) {
         return service.register(request);
+    }
+
+    @GetMapping("/{id}")
+    public DriverResponse getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping
+    public List<DriverResponse> getAll() {
+        return service.getAll();
+    }
+
+    @PatchMapping("/{id}")
+    public DriverResponse update(@PathVariable Long id, @Valid @RequestBody DriverUpdateRequest request) {
+        return service.update(id, request);
+    }
+
+    @PatchMapping("/{id}/status")
+    public void changeStatus(@PathVariable Long id, @RequestParam boolean isAvailable) {
+        service.updateStatus(id, isAvailable);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 }
