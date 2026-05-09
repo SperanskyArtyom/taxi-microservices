27ad51e089f79261444038ce40f60be96b06d28f
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 11:47:14 +0700

    feat: add toBuilder method in entities

--- user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/domain/entity/Passenger.java
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/domain/entity/Passenger.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
+@Builder(toBuilder = true)
 public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
