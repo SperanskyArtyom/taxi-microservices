a6ee2815682df9427c32fbe5ff65a17ffff384a5
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 11:24:40 +0700

    feat: add driver and passenger update request dto

---
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/dto/PassengerUpdateRequest.java
@@ -0,0 +1,32 @@
+package io.github.speranskyatryom.taxi_microservices.user_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerUpdateRequest {
+    @Size(max = 50, message = "First name must be less then 50 characters")
+    private String firstName;
+
+    @Size(max = 50, message = "Last name must be less then 50 characters")
+    private String lastName;
+
+    @Size(max = 100, message = "Email must be less then 100 characters")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(max = 20, message = "Phone must be less then 20 characters")
+    @Pattern(
+            regexp = "^\\+?[1-9]\\d{10,14}$",
+            message = "Invalid phone number format"
+    )
+    private String phone;
+}
