1ea1e951e45ae9ab44575ffa268045d43f283cab
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 11:24:04 +0700

    feat: add messages for fields size validation errors

--- user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/dto/DriverRegistrationRequest.java
+++ user-service/src/main/java/io/github/speranskyatryom/taxi_microservices/user_service/dto/DriverRegistrationRequest.java
@@ -14,21 +14,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DriverRegistrationRequest {
-    @NotBlank(message = "Firs name is required")
-    @Size(max = 50)
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name must be less then 50 characters")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
-    @Size(max = 50)
+    @Size(max = 50, message = "Last name must be less then 50 characters")
     private String lastName;
 
     @NotBlank(message = "Email is required")
-    @Size(max = 100)
+    @Size(max = 100, message = "Email must be less then 100 characters")
     @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Phone is required")
-    @Size(max = 20)
+    @Size(max = 20, message = "Phone must be less then 20 characters")
     @Pattern(
             regexp = "^\\+?[1-9]\\d{10,14}$",
             message = "Invalid phone number format"
@@ -36,6 +36,6 @@ public class DriverRegistrationRequest {
     private String phone;
 
     @NotBlank(message = "Car information is required")
-    @Size(max = 255)
+    @Size(max = 255, message = "Car information must be less then 255 characters")
     private String carInfo;
 }
