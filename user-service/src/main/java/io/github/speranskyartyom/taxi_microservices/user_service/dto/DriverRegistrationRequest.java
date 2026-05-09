4e9eb19ed1ca307139d90cccb137bfd87ac0d1d7
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-10 00:36:28 +0700

    fix: fix typo error in error message

--- user-service/src/main/java/io/github/speranskyartyom/taxi_microservices/user_service/dto/DriverRegistrationRequest.java
+++ user-service/src/main/java/io/github/speranskyartyom/taxi_microservices/user_service/dto/DriverRegistrationRequest.java
@@ -15,20 +15,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DriverRegistrationRequest {
     @NotBlank(message = "First name is required")
-    @Size(max = 50, message = "First name must be less then 50 characters")
+    @Size(max = 50, message = "First name must be less than 50 characters")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
-    @Size(max = 50, message = "Last name must be less then 50 characters")
+    @Size(max = 50, message = "Last name must be less than 50 characters")
     private String lastName;
 
     @NotBlank(message = "Email is required")
-    @Size(max = 100, message = "Email must be less then 100 characters")
+    @Size(max = 100, message = "Email must be less than 100 characters")
     @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Phone is required")
-    @Size(max = 20, message = "Phone must be less then 20 characters")
+    @Size(max = 20, message = "Phone must be less than 20 characters")
     @Pattern(
             regexp = "^\\+?[1-9]\\d{10,14}$",
             message = "Invalid phone number format"
@@ -36,6 +36,6 @@ public class DriverRegistrationRequest {
     private String phone;
 
     @NotBlank(message = "Car information is required")
-    @Size(max = 255, message = "Car information must be less then 255 characters")
+    @Size(max = 255, message = "Car information must be less than 255 characters")
     private String carInfo;
 }
--- user-service/src/main/java/io/github/speranskyartyom/taxi_microservices/user_service/dto/PassengerRegistrationRequest.java
+++ user-service/src/main/java/io/github/speranskyartyom/taxi_microservices/user_service/dto/PassengerRegistrationRequest.java
@@ -15,19 +15,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PassengerRegistrationRequest {
     @NotBlank(message = "First name is required")
-    @Size(max = 50, message = "First name must be less then 50 characters")
+    @Size(max = 50, message = "First name must be less than 50 characters")
     private String firstName;
 
-    @Size(max = 50, message = "Last name must be less then 50 characters")
+    @Size(max = 50, message = "Last name must be less than 50 characters")
     private String lastName;
 
     @NotBlank(message = "Email is required")
-    @Size(max = 100, message = "Email must be less then 100 characters")
+    @Size(max = 100, message = "Email must be less than 100 characters")
     @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Phone is required")
-    @Size(max = 20, message = "Phone must be less then 20 characters")
+    @Size(max = 20, message = "Phone must be less than 20 characters")
     @Pattern(
             regexp = "^\\+?[1-9]\\d{10,14}$",
             message = "Invalid phone number format"
