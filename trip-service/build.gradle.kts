efb04e05d807fbb14f02e5190ed2e970d937cb23
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-09 22:48:06 +0700

    chore: add trip-service configuration to Gradle and init trips table migration in trips_db

---
+++ trip-service/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-liquibase")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.1.0")
+        }
+    }
+}
