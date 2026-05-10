eba86ac71890d20bc13a8552edf8d3d2dd0ff218
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-11 05:40:18 +0700

    feat: implement location validating and price calculation

--- trip-service/src/main/java/io/github/speranskyartyom/taxi_microservices/trip_service/dto/TripCreateRequest.java
+++ trip-service/src/main/java/io/github/speranskyartyom/taxi_microservices/trip_service/dto/TripCreateRequest.java
@@ -1,5 +1,6 @@
 package io.github.speranskyartyom.taxi_microservices.trip_service.dto;
 
+import io.github.speranskyartyom.taxi_microservices.trip_service.validation.IsLocation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,9 +19,11 @@ public class TripCreateRequest {
 
     @NotBlank(message = "Origin is required")
     @Size(max = 255, message = "Origin must be less than 255 characters")
+    @IsLocation
     private String origin;
 
     @NotBlank(message = "Destination is required")
     @Size(max = 255, message = "Destination must be less than 255 characters")
+    @IsLocation
     private String destination;
 }
