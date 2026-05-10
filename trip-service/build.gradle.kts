101936d8ab2016249efa21d427fdbfa79216c2ae
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-11 03:25:47 +0700

    chore: add notification-service project and integrate it into gradle and docker

--- trip-service/build.gradle.kts
+++ trip-service/build.gradle.kts
@@ -1,10 +1,9 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
     runtimeOnly("org.postgresql:postgresql")
--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
