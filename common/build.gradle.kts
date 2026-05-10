f40dd931816306af0234a4c7c2c8c9ead4a32f67
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-10 20:28:09 +0700

    feat: add base ErrorResponse dto to common lib

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
 }
 
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
