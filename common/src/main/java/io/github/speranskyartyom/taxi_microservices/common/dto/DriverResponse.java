1da53b96c097484293c2f80f5da57950767cf0aa
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-10 20:29:26 +0700

    fix: add NoArgsConstructor to DTOs

--- common/src/main/java/io/github/speranskyartyom/taxi_microservices/common/dto/DriverResponse.java
+++ common/src/main/java/io/github/speranskyartyom/taxi_microservices/common/dto/DriverResponse.java
@@ -1,12 +1,16 @@
 package io.github.speranskyartyom.taxi_microservices.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DriverResponse {
     private Long id;
     private String firstName;
