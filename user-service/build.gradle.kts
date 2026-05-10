a938a1ccf9992f5e4d4972a600347de3acc78213
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-05-11 05:06:59 +0700

    chore: add redis container and add it to user-service build configuration

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
